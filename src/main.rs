e4b7ede8eb51d1c00782e988dc59523a9b79d769
Jay Valentine <jayv136@gmail.com>
2026-05-09 23:01:45 +0100

    Tidy up error type usage in `main`

--- src/main.rs
+++ src/main.rs
@@ -11,22 +11,37 @@ mod data;
 
 use model::world::World;
 
+#[derive(Debug)]
+enum AppError {
+    InitialisationError
+}
+
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn main() -> Result<(), AppError> {
     tracing_subscriber::fmt::init();
 
-    let rooms = data::get_rooms("data/rooms/rooms.json").unwrap_or_else(|e| {
+    let rooms = data::get_rooms("data/rooms/rooms.json").map_err(|e| {
         tracing::error!("Error loading room data: {e}");
-        panic!("Initialisation failed!");
-    });
+        AppError::InitialisationError
+    })?;
 
     let world = Arc::new(World::new(rooms));
 
-    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+    let listener = TcpListener::bind("127.0.0.1:8080").await.map_err(|e| {
+        tracing::error!("Error starting TCP listener: {e}");
+        AppError::InitialisationError
+    })?;
     tracing::info!("Listening on port 8080");
 
     loop {
-        let (mut socket, addr) = listener.accept().await?;
+        let result = listener.accept().await;
+        if result.is_err() {
+            let e = result.unwrap_err();
+            tracing::error!("Error handling new connection: {e}");
+            continue;
+        }
+
+        let (mut socket, addr) = result.unwrap();
         tracing::info!("Handling connection from {addr}");
 
         let world = world.clone();
