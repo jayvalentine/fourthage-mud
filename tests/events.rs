fc0c4560270c0245e10e0243490c4752dfddfef0
Jay Valentine <jayv136@gmail.com>
2026-06-15 21:27:51 +0100

    Resolve warnings in test code

--- tests/events.rs
+++ tests/events.rs
@@ -5,8 +5,8 @@ use crate::common::{TestServer, create_test_account};
 
 #[sqlx::test(migrations = "./migrations")]
 async fn test_say_message(pool: PgPool) {
-    create_test_account(&pool, "player1", "password1", false).await;
-    create_test_account(&pool, "player2", "password2", false).await;
+    create_test_account(&pool, "player1", "password1", false).await.expect("player1 account creation failed");
+    create_test_account(&pool, "player2", "password2", false).await.expect("player2 account creation failed");
 
     let server = TestServer::start(&pool).await;
 
